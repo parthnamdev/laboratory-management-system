9d5d0517fc4cee8de618f943ef5f778bf870f9d8
parthnamdev <parthnamdev@users.noreply.github.com>
2021-07-17 17:37:47 +0530

    basic nav

--- labory/controllers/homeController.js
+++ labory/controllers/homeController.js
@@ -2,6 +2,27 @@ const index = (req, res) => {
     res.render('index');
 }
 
-module.exports = {
-    index
+const addPatientPage = (req, res) => {
+    res.render('addPatient');
 }
+
+const managePatient = (req, res) => {
+    res.render('managePatient');
+}
+
+const manageTest = (req, res) => {
+    res.render('manageTest');
+}
+
+const sendReport = (req, res) => {
+    res.render('sendReport');
+}
+
+const payments = (req, res) => {
+    res.render('payments');
+}
+
+module.exports = {
+    index, addPatientPage, managePatient, manageTest, sendReport, payments
+}
+
--- labory/routers/homeRouter.js
+++ labory/routers/homeRouter.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.index);
+router.get('/addPatient', homeController.addPatientPage);
+router.get('/managePatient', homeController.managePatient);
+router.get('/manageTest', homeController.manageTest);
+router.get('/sendReport', homeController.sendReport);
+router.get('/payments', homeController.payments);
 
 
 module.exports = router;
